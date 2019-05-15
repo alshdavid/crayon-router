d7c2980f9d04c1406878161ea2f43361c7154ad7
David Alsh <david.alsh@zipmoney.com.au>
2019-05-15 14:44:57 +1000

    added sandbox for local testing

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,3 @@ export * from './response'
 export * from './types'
 export * from './url'
 export * from './animate'
-
-import { Router, create } from './router'
-import { Group, group } from './group'
-import { Request } from './request'
-import { Response } from './response'
-import { AnimationState, AnimationRoute } from './types'
-import { matchPath, deserializeQuery } from './url'
-import { animate, animation } from './animate'
-
-export default {
-    create,
-    group,
-    animate,
-    animation,
-    Router,
-    Group,
-    Request,
-    Response,
-    AnimationState,
-    AnimationRoute,
-    matchPath,
-    deserializeQuery
-}
