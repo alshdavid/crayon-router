a6188acbf38d0c8a88966305965d310eb4088fd9
David Alsh <alshdavid@gmail.com>
2019-07-26 15:26:01 +1000

    small bug with it

--- src/react/src/with-context.ts
+++ src/react/src/with-context.ts
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Request, Response, Router } from 'crayon';
 
-export const withContext = (context: any, state: Record<string, any>) => {
-  const apply = (Component: any) => React.createElement(context.provider, { value: state }, 
-    React.createElement(Component, null)
-  )
-    return (req: Request, res: Response, state: Record<string, any>, app: Router) => {
-    const original = res.mount
+export const withContext = (context: React.Context<any>, contextState: Record<string, any>) => {
+  const apply = (Component: any) => () =>
+    React.createElement(context.Provider, { value: contextState }, 
+      React.createElement(Component, null))
+
+  return (req: Request, res: Response, state: Record<string, any>, app: Router) => {
+    const mount = res.mount
     res.mount = (c: any) => {
-      original(apply(c))
+      mount(apply(c))
     }
   }
 }
