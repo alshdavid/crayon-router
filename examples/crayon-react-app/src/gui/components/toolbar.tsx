99225d2c60878f352fe361298b33d65ae577465f
David <alshdavid@gmail.com>
2019-08-19 21:16:35 +1000

    removed example folders

--- examples/crayon-react-app/src/gui/components/toolbar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Toolbar: any = React.memo(({ children }: any) => (
-    <div className="toolbar-component">
-        { children }
-    </div>
-))
--- examples/react-animated/src/pages/index.js
+++
@@ -1,2 +0,0 @@
-export * from './more'
-export * from './route'
