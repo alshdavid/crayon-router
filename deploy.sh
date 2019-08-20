8058622ef056306d11dce183357fc8e81a95f2b0
David Alsh <alshdavid@gmail.com>
2019-08-20 11:08:08 +1000

    more CI things

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-yarn
 yarn test:cover
 yarn clean
 yarn build
 if [ ! -z "$NPM_TOKEN" ] 
 then
-    echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > dist/.npmrc
+    echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 fi
-cd dist 
 npm publish
 cd ..
