5132e44932ff924a33089d6041284628b399c9b9
David Alsh <alshdavid@gmail.com>
2019-09-09 13:21:15 +1000

    removed console logs

--- src/react/src/react.ts
+++ src/react/src/react.ts
@@ -9,15 +9,12 @@ export const router = (
 ): handlerFunc => (req, res, state) => {
   if (state.reactState === undefined) {
     state.reactState = new ReactState(target, className)
-    console.log(state.reactState)
   }
   res.mount = (Component: any): Promise<any> => {
     const animationName = AccessSafe.exec(
       () => res.ctx.animation.name, undefined)
     const animationDuration = AccessSafe.exec(
       () => res.ctx.animation.duration, undefined)
-
-      console.log(state.reactState.mounter)
     return mount(
       Component,
       state.reactState.mounter,
