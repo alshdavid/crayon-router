f3977231ab8953234796b68648e23dd8485ea789
David Alsh <alshdavid@gmail.com>
2019-09-25 14:15:59 +1000

    reexporting RouterEventType from package crayon

--- src/crayon/src/index.ts
+++ src/crayon/src/index.ts
@@ -4,11 +4,14 @@ export * from './platform/history'
 export * from './platform/locator'
 export * from './platform/route-map'
 export * from './platform/shared-state'
+export * from './platform/router'
 export * from './sdk'
 
-
 import { create, group } from './sdk'
-import { Router as OGRouter, Group as OGGroup } from './platform/router'
+import { 
+  Router as OGRouter, 
+  Group as OGGroup,
+} from './platform/router'
 
 export const crayon = {
   create, 
